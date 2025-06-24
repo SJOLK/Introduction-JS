74dcb2bc2b5511c417d344d9b3c7f30195ab567e
SJOLK <omarrh0645@gmail.com>
2025-06-24 10:47:22 +0200

    Initial commit

---
+++ tp2.js
@@ -0,0 +1,10 @@
+// Déclaration des deux variables entières
+let num1 = 10;
+let num2 = 5;
+
+// Affichage des résultats des opérations
+console.log("Addition : " + (num1 + num2));         // 15
+console.log("Soustraction : " + (num1 - num2));     // 5
+console.log("Multiplication : " + (num1 * num2));   // 50
+console.log("Division : " + (num1 / num2));         // 2
+console.log("Modulo : " + (num1 % num2));           // 0
